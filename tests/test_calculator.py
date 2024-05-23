fda1c382d9a047159ac9e72592b62da27deee166
Best-Jason <23022483@myrp.edu.sg>
2024-05-23 11:55:55 +0800

    initial commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,66 @@
+from calculator.calculator import Calculator
+import pytest
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+    def test_subtract(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.subtract(a, b)
+
+        # assert
+        expected = 3087
+        assert result == expected
+
+
+    def test_multiply(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.multiply(a, b)
+
+        # assert
+        expected = 5332114
+        assert result == expected
+
+
+    def test_divide(self):
+        # arrange
+        a = 10
+        b = 2
+        cal = Calculator()
+
+    # assert
+        cal.divide(a,b)
+
+
+    def test_dividebyzero(self):
+        # arrange
+        a = 8
+        b = 0
+        cal = Calculator()
+
+
+
+    # assert
+        with pytest.raises(ZeroDivisionError):
+            cal.divide(a,b)
